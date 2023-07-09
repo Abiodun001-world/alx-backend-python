438695d633424020c22353a58a0254ec7f703eda
Abiodun001-wold <adekunleabiodun354@gmail.com>
2023-07-09 02:29:03 -0400

    fixed a blank line at the end of file

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -1,12 +1,15 @@
-#!/usr/bin/env python3
-'''Complex types=list of floats
-'''
 from typing import List
 
-
 def sum_list(input_list: List[float]) -> float:
-    ''' Takes a list input_list of floats as argument
-    returns their sum as a float.
-    '''
-    return float(sum(input_list))
+    """
+    Calculates the sum of a list of floats.
+
+    Args:
+        input_list: A list of floats.
+
+    Returns:
+        The sum of the floats in the input list.
+    """
+    return sum(input_list)
+
 
